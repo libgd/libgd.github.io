cf8ab32ae06784ef8d13b68006003c06ab0abe97
Pierre Joye <pierre.php@gmail.com>
2026-07-16 00:10:41 +0700

    Generate Pelican site

--- manuals/2.4.0-dev/search/groups_7.js
+++ manuals/2.4.0-dev/search/groups_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['gd_0',['GD',['../group__gdCodecGd.html',1,'']]],
-  ['gif_1',['GIF',['../group__gdCodecGif.html',1,'']]]
+  ['gd2_1',['GD2',['../group__gdCodecGd2.html',1,'']]],
+  ['gif_2',['GIF',['../group__gdCodecGif.html',1,'']]]
 ];
